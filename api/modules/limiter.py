905f8437a91e5355230f64a0bdaae2439ba63b29
Edgars <edgars.gogolevs@gmail.com>
2025-03-10 22:44:16 +0200

    better rate limit

--- api/modules/limiter.py
+++ api/modules/limiter.py
@@ -4,6 +4,6 @@ from flask_limiter.util import get_remote_address
 # Limit by IP is used, realistically limit per user would be better.
 limiter = Limiter(
   get_remote_address,
-  default_limits=["200 per day", "50 per hour", "1 per second"],
+  default_limits=["1000 per day", "200 per hour", "4 per second"],
   storage_uri="memory://",  # Memory storage used for simplicity, in real life use Redis or smth like that
 )
